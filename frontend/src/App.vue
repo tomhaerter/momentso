ec05839ed2445a3e8bfc8ca007f79069f3523cdb
Tom Härter <tom@dokedu.org>
2023-09-26 20:48:50 +0200

    hide login and logout buttons on home screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,10 +12,8 @@ async function signOut() {
 </script>
 
 <template>
-  <nav class="px-4 py-2 flex gap-2" v-if="$route.name !== 'signup' && $route.name !== 'login'">
+  <nav class="px-4 py-2 flex gap-2 justify-between" v-if="$route.name !== 'signup' && $route.name !== 'login'">
     <RouterLink class="p-2 bg-white/5 hover:bg-white/10 rounded-xl text-sm" to="/">Home</RouterLink>
-    <RouterLink class="p-2 bg-white/5 hover:bg-white/10 rounded-xl text-sm" to="/login">Login</RouterLink>
-    <RouterLink class="p-2 bg-white/5 hover:bg-white/10 rounded-xl text-sm" to="/signup">Sign up</RouterLink>
     <button @click="signOut" class="p-2 bg-white/5 hover:bg-white/10 rounded-xl text-sm" to="/signup">Sign out</button>
   </nav>
 
